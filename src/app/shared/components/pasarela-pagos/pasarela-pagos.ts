f415f2c0a2041c085dd0d86b336a557d6042c035
Javier <javierts253@gmail.com>
2026-03-16 17:29:00 -0500

    ajustes en disenio

--- src/app/shared/components/pasarela-pagos/pasarela-pagos.ts
+++ src/app/shared/components/pasarela-pagos/pasarela-pagos.ts
@@ -72,10 +72,9 @@ export class PasarelaPagos implements OnInit {
     });
 
     this.niubizService.getSession(this.nMonto, this.lstIdentif).subscribe((res:any)=>{
-      console.log('PRUEBAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA');
       this.sessionResponse = res as SessionResponse;
-      // this.urlRespuestaNiubiz = "http://localhost:8085/pago-online/niubiz/callback/" + (this.sessionResponse?.purchaseNumber || '');
-      this.urlRespuestaNiubiz = "http://192.168.0.61:8085/pago-online/niubiz/callback/" + (this.sessionResponse?.purchaseNumber || '');
+      this.urlRespuestaNiubiz = "http://localhost:8085/pago-online/niubiz/callback/" + (this.sessionResponse?.purchaseNumber || '');
+      //this.urlRespuestaNiubiz = "http://192.168.0.61:8085/pago-online/niubiz/callback/" + (this.sessionResponse?.purchaseNumber || '');
       setTimeout(()=>{
         this.loadNiubizScript();
       }, 150)
