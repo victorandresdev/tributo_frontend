defcf67fb969ff2de36d82d13bdd63be7de68b1c
jtapias <javierts253@gmail.com>
2026-09-24 00:12:18 -0500

    consumo de servicio local

--- src/app/shared/components/pasarela-pagos/pasarela-pagos.ts
+++ src/app/shared/components/pasarela-pagos/pasarela-pagos.ts
@@ -80,8 +80,8 @@ export class PasarelaPagos implements OnInit, AfterViewInit, OnDestroy {
 
     this.niubizService.getSession(this.nMonto, this.lstIdentif).subscribe((res:any)=>{
       this.sessionResponse = res as SessionResponse;
-      //this.urlRespuestaNiubiz = "http://localhost:8085/pago-online/niubiz/callback/" + (this.sessionResponse?.purchaseNumber || '');
-      this.urlRespuestaNiubiz = "http://192.168.0.61:8085/pago-online/niubiz/callback/" + (this.sessionResponse?.purchaseNumber || '');
+      this.urlRespuestaNiubiz = "http://localhost:8085/pago-online/niubiz/callback/" + (this.sessionResponse?.purchaseNumber || '');
+      //this.urlRespuestaNiubiz = "http://192.168.0.61:8085/pago-online/niubiz/callback/" + (this.sessionResponse?.purchaseNumber || '');
       this.cargarScriptCuandoEsteListo();
     });
 
