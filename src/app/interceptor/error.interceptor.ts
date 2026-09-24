430ca00a63ef072fb4dcac14df3d871b67f42010
jtapias <javierts253@gmail.com>
2026-09-24 01:14:18 -0500

    avances

--- src/app/interceptor/error.interceptor.ts
+++ src/app/interceptor/error.interceptor.ts
@@ -2,9 +2,21 @@ import { HttpInterceptorFn, HttpErrorResponse } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { catchError, throwError } from 'rxjs';
+import { UtilService } from '../services/util.services';
+import { MatDialog } from '@angular/material/dialog';
+import { APP_CONSTANTS } from '../shared/constants/app.constants';
+import { APP_ROUTES } from '../shared/constants/app.routes';
+import Swal from 'sweetalert2';
+
+const ENDPOINTS_PUBLICOS_401 = ['loginGeneral', 'login-persona', 'login-admin', '/token'];
 
 export const globalErrorInterceptor: HttpInterceptorFn = (req, next) => {
   const snackBar = inject(MatSnackBar);
+  const utilService = inject(UtilService);
+  const dialog = inject(MatDialog);
+
+  const esEndpointPublico = ENDPOINTS_PUBLICOS_401.some(segmento => req.url.includes(segmento));
+
   return next(req).pipe(
     catchError((error: HttpErrorResponse) => {
       let msg = 'Error desconocido';
@@ -12,6 +24,23 @@ export const globalErrorInterceptor: HttpInterceptorFn = (req, next) => {
         msg = '❌ Error el servidor no está disponible.';
       } else if (error.status === 400) {
         msg = 'ERROR 400';
+      } else if (error.status === 401) {
+        const hayToken = !!utilService.getLocalStorage(APP_CONSTANTS.VAR_TOKEN);
+
+        if (!esEndpointPublico && hayToken) {
+          Swal.fire({
+            title: 'Sesión terminada',
+            text: 'Su sesión ha expirado o no tiene autorización.',
+            icon: 'info',
+            confirmButtonText: 'Entendido'
+          });
+          utilService.removeLocalStorage(APP_CONSTANTS.VAR_TOKEN);
+          utilService.removeLocalStorage(APP_CONSTANTS.VAR_USUARIO);
+          dialog.closeAll();
+          utilService.link(APP_ROUTES.URL_HOME);
+        }
+
+        return throwError(() => error);
       } else if (error.status === 403) {
         msg ='🚫 Posible problema con la sesión, se recomienda volver a iniciar sesión.';
       } else if (error.status === 404) {
@@ -24,7 +53,7 @@ export const globalErrorInterceptor: HttpInterceptorFn = (req, next) => {
         duration: 5000,
         horizontalPosition: 'right',
         verticalPosition: 'top',
-        panelClass: ['snackbar-error'] // opcional
+        panelClass: ['snackbar-error']
       });
 
       return throwError(() => error);
